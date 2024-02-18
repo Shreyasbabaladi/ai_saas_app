45021d2bbdc1786457211b5c0220ab5c70dc09b4
shreyas-awako <43786693+Shreyasbabaladi@users.noreply.github.com>
2024-02-18 23:29:37 +0530

    Implemented root routers

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreditPage = () => {
+  return <main className="credits">CreditsPage</main>;
+};
+
+export default CreditPage;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const updateTransformationsPage = () => {
+  return <div>updateTransformationsPage</div>;
+};
+
+export default updateTransformationsPage;
